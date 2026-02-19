54713b2b3ac004d2ecc504327a77039a45285301
BrianBantog <bbantog376.pbox@parsu.edu.ph>
2026-02-20 00:35:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // ===== Pokémon List =====
 const pokemons = [
-    { name: "Pikachu", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" },
+  
+  { name: "Pikachu", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" },
+
     { name: "Bulbasaur", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" },
+
     { name: "Charmander", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png" },
+
     { name: "Squirtle", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png" },
+
     { name: "Eevee", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png" },
+
     { name: "Jigglypuff", img:
 "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png" },
+
 ];
+
 
 // ===== Game Variables =====
 let secretPokemon;
